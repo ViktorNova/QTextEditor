5511e081c0cbf521087265bc2e34968153aed059
Viktor Nova <ViktorNova@users.noreply.github.com>
2015-01-13 17:29:02 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    a.setApplicationName("Text Editor");
+    a.setApplicationName("QTextEditor");
     a.setApplicationVersion("0.0.2");
     a.setOrganizationName("Progra III Inc.");
     a.setOrganizationDomain("unitec.edu");
